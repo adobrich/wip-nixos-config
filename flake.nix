92c41156a3856d6a4cd4977f90e56a6f7927b048
Andrew Dobrich <andrew.dobrich@proton.me>
2026-06-21 21:21:07 +1000

    restructure

--- flake.nix
+++ flake.nix
@@ -1,21 +1,21 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flakelight.url = "github:nix-community/flakelight";
+
     preservation.url = "github:nix-community/preservation";
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }: {
-    nixosConfigurations.nixos-zfs-vm = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        inputs.disko.nixosModules.disko
-        ./configuration.nix
-        ./disko-configuration.nix
-        ./hardware-configuration.nix
-      ];
-    };
+  outputs = { flakelight, ... }@inputs: {
+    flakelight ./. {
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      inherit inputs;
   };
 }
 
